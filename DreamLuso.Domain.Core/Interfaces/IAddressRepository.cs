b1a3596dc109b2281975a1776ea00d30c364e308
Rafael Oliveira <rafael.oliveira@students.assembly.pt>
2024-09-09 14:46:07 +0100

    commit test

--- DreamLuso.Domain.Core/Interfaces/IAddressRepository.cs
+++ DreamLuso.Domain.Core/Interfaces/IAddressRepository.cs
@@ -5,4 +5,5 @@ namespace DreamLuso.Domain.Core.Interfaces;
 public interface IAddressRepository : IRepository<Address, Guid>
 {
     Task<Address> GetByFullAddressAsync(Address address, CancellationToken cancellationToken = default);
+
 }
